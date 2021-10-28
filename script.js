5cd68ae7a253c1833b14f3c2b7cd7f45b29527fb
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2021-10-28 14:50:15 +0200

    Update the logo animation and DOM structure

--- script.js
+++ script.js
@@ -58,46 +58,49 @@
         ],
     };
 
-    const FADE_IN = [
-        { opacity: 0 },
-        { opacity: 1 },
+    const FADE_SLIDE_IN = [
+        { opacity: 0, transform: 'translate3D(0, -100%, 0)' },
+        { opacity: 1, transform: 'translate3D(0, 0, 0)' },
     ];
-    const FADE_IN_TIMING = {
+    const FADE_SLIDE_IN_TIMING = {
         duration: 100,
         fill: 'forwards',
     };
 
     function loadLogo() {
         const chars = document.querySelectorAll('.char');
-        const charsMarkedBoxes = Array.from(chars).map(generateChar);
-        charsMarkedBoxes.forEach(fadeInChar);
+        const charsMarkedCells = Array.from(chars).map(generateChar);
+        charsMarkedCells.forEach(showChar);
     }
 
     function generateChar(char) {
-        const markedBoxes = [];
+        const markedCells = [];
         const charTable = CHARS_TABLE[char.dataset.char.toUpperCase()];
         // eslint-disable-next-line no-restricted-syntax
         for (const row of charTable) {
+            const rowContainer = document.createElement('div');
+            rowContainer.classList.add('row');
             // eslint-disable-next-line no-restricted-syntax
             for (const column of row) {
-                const box = document.createElement('span');
-                box.classList.add('box');
+                const cell = document.createElement('span');
+                cell.classList.add('cell');
                 if (column) {
-                    box.classList.add('marked');
-                    markedBoxes.push(box);
+                    cell.classList.add('marked');
+                    markedCells.push(cell);
                 }
-                char.appendChild(box);
+                rowContainer.appendChild(cell);
             }
+            char.appendChild(rowContainer);
         }
-        return markedBoxes;
+        return markedCells;
     }
 
-    async function fadeInChar(charMarkedBoxes) {
+    async function showChar(charMarkedBoxes) {
         // eslint-disable-next-line no-restricted-syntax
         for (const markedBox of charMarkedBoxes) {
             // Wait for an animation to finish before moving to the next markedBox
             // eslint-disable-next-line no-await-in-loop
-            await markedBox.animate(FADE_IN, FADE_IN_TIMING).finished;
+            await markedBox.animate(FADE_SLIDE_IN, FADE_SLIDE_IN_TIMING).finished;
         }
     }
 
